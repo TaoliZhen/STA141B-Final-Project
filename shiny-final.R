c6c0f7728698aee5aa80f75310b15277bc871793
Taoli Zhen <tazhen@ucdavis.edu>
2020-06-04 03:04:48 -0700

    6/4

--- shiny-final.R
+++ shiny-final.R
@@ -47,8 +47,94 @@ get_minly <- function(syb){
   df
 }
 
+get_daily <- function(syb){
+  r <- GET(
+    "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY",
+    query = list(
+      symbol = syb,
+      outputsize = "full",
+      apikey = Sys.getenv("AVS_API_KEY")
+    )
+  )
+  stop_for_status(r)
+  json <- content(r, as = "text", encoding = "UTF-8")
+  df_daily <- fromJSON(json, flatten = TRUE) %>% 
+    as.data.frame() %>%
+    select(symbol = ends_with("Symbol"), ends_with("close")) %>% 
+    pivot_longer(
+      starts_with("Time.Series..Daily.."),
+      names_to = "Date",
+      names_prefix = "Time.Series..Daily.."
+    ) %>% 
+    separate(col = "Date", c("year", "month","date"), sep = "[.]") %>% 
+    unite(col = "Date", 2:4, sep = "/") %>% 
+    mutate(
+      Date = gsub("^.........$","", Date),
+      Date = as.Date(Date),
+      Price = as.numeric(as.character(value))
+    )
+  df_daily
+}
 
+get_wma5 <- function(syb){
+  r <- GET(
+    "https://www.alphavantage.co/query?function=WMA",
+    query = list(
+      symbol = syb,
+      interval = "daily",
+      time_period = 5,
+      series_type = "close",
+      apikey = Sys.getenv("AVS_API_KEY")
+    )
+  )
+  stop_for_status(r)
+  json <- content(r, as = "text", encoding = "UTF-8")
+  df_wma5 <- fromJSON(json, flatten = TRUE) %>% 
+    as.data.frame() %>%
+    select(symbol = ends_with("Symbol"), time_point = ends_with("Type"), starts_with("Technical.Analysis")) %>% 
+    pivot_longer(
+      starts_with("Technical.Analysis"),
+      names_to = "ma_lag",
+      names_prefix = "Technical.Analysis..WMA."
+    ) %>% 
+    head(91) %>% 
+    separate(col = "ma_lag", c("indicator", "lag"), sep = "[.]") %>% 
+    mutate(lag = replace_na(lag, "0"),
+           lag = as.numeric(lag),
+           value = as.numeric(as.character(value))
+    )
+  df_wma5
+}
 
+get_wma30 <- function(syb){
+  r <- GET(
+    "https://www.alphavantage.co/query?function=WMA",
+    query = list(
+      symbol = syb,
+      interval = "daily",
+      time_period = 30,
+      series_type = "close",
+      apikey = "G3O0P1B2VGOZYKZN"
+    )
+  )
+  stop_for_status(r)
+  json <- content(r, as = "text", encoding = "UTF-8")
+  df_wma30 <- fromJSON(json, flatten = TRUE) %>% 
+    as.data.frame() %>%
+    select(symbol = ends_with("Symbol"), time_point = ends_with("Type"), starts_with("Technical.Analysis")) %>% 
+    pivot_longer(
+      starts_with("Technical.Analysis"),
+      names_to = "ma_lag",
+      names_prefix = "Technical.Analysis..WMA."
+    ) %>% 
+    head(91) %>% 
+    separate(col = "ma_lag", c("indicator", "lag"), sep = "[.]") %>% 
+    mutate(lag = replace_na(lag, "0"),
+           lagf = as.numeric(lag),
+           valuef = as.numeric(as.character(value))
+    )
+  df_wma30
+}
 
 ui <- fluidPage(
   
@@ -62,19 +148,18 @@ ui <- fluidPage(
 
       textInput(inputId = "sb",
         label = "Enter Stock Symbol",
-        value = "AAPL"),
+        value = "PDD"),
       actionButton(inputId ="go", label = "Show Results")
     ),
   
   # Output panel
     mainPanel(
       tabsetPanel(
-        tabPanel("1", plotOutput("one"), verbatimTextOutput("onetwo")),
-        tabPanel("2", plotOutput("two"), verbatimTextOutput("twotwo")),
-        tabPanel("3", plotOutput("three"), verbatimTextOutput("threetwo")),
-        tabPanel("4", plotOutput("four"), verbatimTextOutput("fourtwo")),
-        tabPanel("5", plotOutput("five")),
-        tabPanel("6", plotOutput("six"))
+        tabPanel("Latest Trade Day", plotOutput("one"), verbatimTextOutput("onetwo")),
+        tabPanel("Six Months", plotOutput("two"), verbatimTextOutput("twotwo")),
+        tabPanel("One Year", plotOutput("three"), verbatimTextOutput("threetwo")),
+        tabPanel("Historic", plotOutput("four"), verbatimTextOutput("fourtwo")),
+        tabPanel("Weighted Moving Average", plotOutput("five"), verbatimTextOutput("fivetwo"))
       ),
       position = c("left"),
       fluid = FALSE
@@ -84,32 +169,132 @@ ui <- fluidPage(
 
 server <- function(input, output) {
 
-
-  data <- eventReactive(input$go, {
+  # Data and Output for the first tap "Latest Trade Day"
+  data_1 <- eventReactive(input$go, {
     dat <- get_symbol(input$sb)
     get_minly(dat$symbol)
   })
-
   output$one <- renderPlot({
     dat <- get_symbol(input$sb)
     if (is.na(dat[1]) == TRUE){
       cancelOutput = TRUE
     } else {
-      ggplot(data(), aes(x = Date, y = Price)) +
+      ggplot(data_1(), aes(x = Date, y = Price)) +
         geom_line()  +
-        ggtitle(paste0("Latest Stock Prices on ", data()$Date[1] ," : ", dat$stock_name)) +
+        ggtitle(paste0("Latest Stock Prices on ", data_1()$Date[1] ,": ", dat$stock_name)) +
         theme(plot.title = element_text(lineheight = 0.7, face = "bold"))
     }
   })
-  
   output$onetwo <- renderPrint({
     dat <- get_symbol(input$sb)
     if (is.na(dat[1]) == TRUE){
       print("Sorry the symbol you enter is not found, please double check and try again.")
     } else {
-      summary(data()$Price)
+      summary(data_1()$Price)
     }
   })
+  
+  # Data and Output for the second tap "Six Months"
+  data_2 <- eventReactive(input$go, {
+    dat <- get_symbol(input$sb)
+    get_daily(dat$symbol)
+  })
+  output$two <- renderPlot({
+    dat <- get_symbol(input$sb)
+    if (is.na(dat[1]) == TRUE){
+      cancelOutput = TRUE
+    } else {
+      ggplot(data_2() %>% head(127), aes(x = Date, y = Price)) +
+        geom_line() +
+        ggtitle(paste0("Stock Prices for the Past 6 Months: ", dat$stock_name)) +
+        theme(plot.title = element_text(lineheight = 1, face = "bold"))
+    }
+  })
+  output$twotwo <- renderPrint({
+    dat <- get_symbol(input$sb)
+    if (is.na(dat[1]) == TRUE){
+      print("Sorry the symbol you enter is not found, please double check and try again.")
+    } else {
+      summary(data_2()$Price %>% head(127))
+    }
+  })
+  
+  # Output for the third tap "One Year"
+  output$three <- renderPlot({
+    dat <- get_symbol(input$sb)
+    if (is.na(dat[1]) == TRUE){
+      cancelOutput = TRUE
+    } else {
+      ggplot(data_2() %>% head(253), aes(x = Date, y = Price)) +
+        geom_line() +
+        ggtitle(paste0("Stock Prices for the Past Year: ", dat$stock_name)) +
+        theme(plot.title = element_text(lineheight = 1, face = "bold"))
+    }
+  })
+  output$threetwo <- renderPrint({
+    dat <- get_symbol(input$sb)
+    if (is.na(dat[1]) == TRUE){
+      print("Sorry the symbol you enter is not found, please double check and try again.")
+    } else {
+      summary(data_2()$Price %>% head(253))
+    }
+  })
+  
+  # Output for the forth tap "Historic"
+  output$four <- renderPlot({
+    dat <- get_symbol(input$sb)
+    if (is.na(dat[1]) == TRUE){
+      cancelOutput = TRUE
+    } else {
+      ggplot(data_2(), aes(x = Date, y = Price)) +
+        geom_line() +
+        ggtitle(paste0("Historic Stock Prices: ", dat$stock_name)) +
+        theme(plot.title = element_text(lineheight = 1, face = "bold"))
+    }
+  })
+  output$fourtwo <- renderPrint({
+    dat <- get_symbol(input$sb)
+    if (is.na(dat[1]) == TRUE){
+      print("Sorry the symbol you enter is not found, please double check and try again.")
+    } else {
+      summary(data_2()$Price)
+    }
+  })
+  
+  # Data and Output for the fifth tap "Weighted Moving Average"
+  data_3 <- eventReactive(input$go, {
+    dat <- get_symbol(input$sb)
+    get_wma5(dat$symbol)
+  })
+  data_4 <- eventReactive(input$go, {
+    dat <- get_symbol(input$sb)
+    get_wma30(dat$symbol)
+  })
+
+  output$five <- renderPlot({
+    dat <- get_symbol(input$sb)
+    if (is.na(dat[1]) == TRUE){
+      cancelOutput = TRUE
+    } else {
+      ggplot() +
+        geom_line(data = data_3(), mapping = aes(x = lag, y = value, color = "WMA5")) +
+        geom_line(data = data_4(), mapping = aes(x = lagf, y = valuef, color = "WMA30")) +
+        scale_x_reverse() +
+        labs(color = "MA Days in Period") +
+        ggtitle(paste0("Weighted Moving Average: ", dat$stock_name)) +
+        theme(plot.title = element_text(lineheight = 1, face = "bold"))
+    }
+  })
+  
+  output$fivetwo <- renderPrint({
+    dat <- get_symbol(input$sb)
+    if (is.na(dat[1]) == TRUE){
+      print("Sorry the symbol you enter is not found, please double check and try again.")
+    } else {
+      "When WMA5 crosses WMA30, it may indicates a buying opportunity"
+    }
+  })
+  
 }
 
 shinyApp(ui = ui, server = server)
